37effd165fb991a705624720a19ad2b21fbb002c
DoDung <doquangdung1782004@gmail.com>
2024-04-24 16:23:15 +0700

    add custom font with google fonts

---
+++ src/screens/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, TextComponent } from "../../components";
+
+const HomeScreen = () => {
+  return (
+    <Container>
+      <TextComponent text="Hello world!!!"/>
+    </Container>
+  );
+};
+
+export default HomeScreen;
